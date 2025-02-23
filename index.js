f9491f3edc42252ff3e414656f819234ca96399e
dennys-terra <dennys.jornadadev@gmail.com>
2025-02-23 14:21:23 -0300

    fix: fix path of images

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const faq = document.querySelectorAll('.duvida')
 
-const openOrClose = faq.forEach((question) => {
-    question.addEventListener('click', () => question.classList.toggle('faqIsActive'))
-});
+const openOrClose = () =>{
+    faq.forEach((question) => {
+        question.addEventListener('click', () => question.classList.toggle('faqIsActive'))
+    });
+} 
 
 openOrClose();
